aa52f1fe1565e4d2165beea7a2cc673ac4c69951
Carl Tablante <carl.q.tablante@gmail.com>
2019-02-01 08:30:15 +0800

    Set image background opacity to 0 while waiting for it to load

--- src/Components/Image/index.js
+++ src/Components/Image/index.js
@@ -1,24 +1,50 @@
 import React, { PureComponent } from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 export default class AppImage extends PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      opacity: 0,
+    }
+  }
+
   imageSource = () => {
     const { source } = this.props;
     return source ? { uri: source } : require('./pacman.jpeg');
   }
 
+  onLoad = () => {
+    this.setState({ opacity: 1 })
+  }
+
   render() {
+    const { opacity } = this.state;
     const {
       style,
       ...rest
     } = this.props;
 
+    const imageOpacity = {
+      opacity,
+    }
+
     return(
-      <Image
-        { ...rest }
-        {...{ style }}
-        source={this.imageSource()}
-      />
+      <View style={styles.container}>
+        <Image
+          { ...rest }
+          style={[imageOpacity, style]}
+          onLoad={this.onLoad}
+          source={this.imageSource()}
+        /> 
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#CCC'
+  },
+})
